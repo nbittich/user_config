799d0065e315f6f6fb2a02c20555af6e3fde6b6a
Nordine Bittich <nordine@bittich.be>
2023-09-17 09:28:17 +0200

    autocommit

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -4,6 +4,10 @@ return {
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       -- "lua"
+      'c', 'cpp', 'go', 'lua', 'python', 'rust', 'php', 'java', 'tsx', 'javascript', 'typescript',
+      'vimdoc',
+      'vue',
+      'vim'     
     })
   end,
 }
