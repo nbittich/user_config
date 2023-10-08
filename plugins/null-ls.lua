c3c686d3b2c8f5f09eb91f656600fd40f8137bfe
Nordine Bittich <nordine@bittich.be>
2023-10-08 21:30:25 +0200

    add prettier for html

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -2,7 +2,10 @@ return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
+    null_ls.register(null_ls.builtins.formatting.prettier.with {
+        extra_filetypes = { "htmldjango"},
+    })
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
